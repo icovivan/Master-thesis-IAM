93a473862d9787f3e17551ad0d89e523cab65434
ivanicov-hub <ivan.icov@student.um.si>
2026-02-07 21:03:24 +0100

    Docker compose test users loading removed

--- LDAP/employee-directory-backend/src/routes/employeesRoutes.ts
+++ LDAP/employee-directory-backend/src/routes/employeesRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import employeesController from '../controllers/employeeController';
+import employeesController from '../controllers/employeesController';
 import { requireAuth } from '../middleware/authMiddleware';
 
 const router = express.Router();
